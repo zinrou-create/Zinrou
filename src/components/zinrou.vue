b871f6e7a6e7a56fbe32a019eab0002356d253d2
tutitona <39608217+tutitona@users.noreply.github.com>
2019-05-02 19:03:40 +0900

    Create zinrou.vue

---
+++ src/components/zinrou.vue
@@ -0,0 +1,12 @@
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+<div id="components-demo">
+  <button-counter></button-counter>
+</div>
+new Vue({ el: '#components-demo' })
